33c78fc9523ea9eddec48d8ac17963f8b771cac3
Ann_Ar0 <ann.artyuh90@gmail.com>
2025-04-09 18:13:25 +0300

    last changes

--- src/validation/contacts.js
+++ src/validation/contacts.js
@@ -13,7 +13,6 @@ export const createContactSchema = Joi.object({
     }
     return true;
   }),
-  photo: Joi.string(),
 });
 
 export const updateContactSchema = Joi.object({
@@ -28,5 +27,4 @@ export const updateContactSchema = Joi.object({
     }
     return true;
   }),
-  photo: Joi.string(),
 });
